7f2a73cf6e9cf2b60fd23a5ee9586f8f5679a266
mariomui <ykanmui@viscira.com>
2020-01-20 04:29:06 -0800

    Allow height and width attributes to be auto

--- src/lottieAnimationView.component.ts
+++ src/lottieAnimationView.component.ts
@@ -1,49 +1,79 @@
-import { Component, Input, OnInit, Output, EventEmitter, ViewChild, ElementRef, PLATFORM_ID, Inject } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  Output,
+  EventEmitter,
+  ViewChild,
+  ElementRef,
+  PLATFORM_ID,
+  Inject
+} from '@angular/core';
 import { isPlatformServer } from '@angular/common';
 
 declare let require: any;
 const lottie: any = require('lottie-web/build/player/lottie.js');
 
 @Component({
-    selector: 'lottie-animation-view',
-    template: `<div #lavContainer 
-                    [ngStyle]="{'width': viewWidth, 'height': viewHeight, 'overflow':'hidden', 'margin': '0 auto'}">    
-               </div>`
+  selector: 'lottie-animation-view',
+  template: `
+    <div
+      #lavContainer
+      [ngStyle]="{
+        width: viewWidth,
+        height: viewHeight,
+        overflow: 'hidden',
+        margin: '0 auto'
+      }"
+    ></div>
+  `
 })
-
 export class LottieAnimationViewComponent implements OnInit {
+  public viewWidth: string;
+  public viewHeight: string;
 
-    public viewWidth: string;
-    public viewHeight: string;
+  @Input() options: any;
+  @Input() width: number;
+  @Input() height: number;
+  @Output() animCreated: any = new EventEmitter();
+  @ViewChild('lavContainer') lavContainer: ElementRef;
 
-    @Input() options: any;
-    @Input() width: number;
-    @Input() height: number;
-    @Output() animCreated: any = new EventEmitter();
-    @ViewChild('lavContainer') lavContainer: ElementRef;
+  private _options: any;
 
-    private _options: any;
-
-    constructor(@Inject(PLATFORM_ID) private platformId: string) { }
-    ngOnInit() {
-
-        if (isPlatformServer(this.platformId)) { return; }
-
-        this._options = {
-            container: this.lavContainer.nativeElement,
-            renderer: this.options.renderer || 'svg',
-            loop: this.options.loop !== false,
-            autoplay: this.options.autoplay !== false,
-            autoloadSegments: this.options.autoloadSegments !== false,
-            animationData: this.options.animationData,
-            path: this.options.path || '',
-            rendererSettings: this.options.rendererSettings || {}
-        };
-
-        this.viewWidth = this.width + 'px' || '100%';
-        this.viewHeight = this.height + 'px' || '100%';
-
-        let anim: any = lottie.loadAnimation(this._options);
-        this.animCreated.emit(anim);
+  constructor(@Inject(PLATFORM_ID) private platformId: string) {}
+  ngOnInit() {
+    if (isPlatformServer(this.platformId)) {
+      return;
     }
+
+    this._options = {
+      container: this.lavContainer.nativeElement,
+      renderer: this.options.renderer || 'svg',
+      loop: this.options.loop !== false,
+      autoplay: this.options.autoplay !== false,
+      autoloadSegments: this.options.autoloadSegments !== false,
+      animationData: this.options.animationData,
+      path: this.options.path || '',
+      rendererSettings: this.options.rendererSettings || {}
+    };
+
+    if (this.isValidDimension(this.height)) {
+      this.viewHeight = `${this.height}px` || '100%';
+    } else {
+      this.viewHeight = 'auto';
+    }
+
+    if (this.isValidDimension(this.width)) {
+      this.viewWidth = `${this.width}px` || '100%';
+    } else {
+      this.viewWidth = 'auto';
+    }
+
+    let anim: any = lottie.loadAnimation(this._options);
+    this.animCreated.emit(anim);
+  }
+
+  isValidDimension(dimension: number) {
+    return !(['autopx', 'nullpx', 'undefinedpx'].indexOf(`${dimension}px`) > -1);
+  }
 }
